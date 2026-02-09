07b00a797eb95ad4b72ffd0086492cd06656d456
Miku-79 <deepaknoob957@gmail.com>
2026-02-09 14:57:48 +0530

    altered models.py and set up alembic

--- app/models/database.py
+++ app/models/database.py
@@ -1,11 +1,11 @@
 from sqlalchemy.orm import DeclarativeBase,sessionmaker
 from sqlalchemy import create_engine
-from app.config import dev_setting
+from app.config import get_config
 
 class Base(DeclarativeBase):
     pass
 
-engine = create_engine(dev_setting.database_uri)
+engine = create_engine(get_config().database_uri)
 SessionLocal = sessionmaker(autoflush=False,autocommit=False,bind=engine)
 
 def get_db():
--- app/models/model.py
+++ app/models/model.py
@@ -7,19 +7,23 @@ import uuid
 from datetime import datetime,timezone
 from app.models.types import FileStatus
 
+
+#for storing user data
 class UsersTable(Base):
     __tablename__ = "users_table"
     
-    id:Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True),primary_key=True,nullable=False,default=uuid.uuid64)
-    email:Mapped[str] = mapped_column(String(255),nullable=False,Unique=True,index=True)
+    id:Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True),primary_key=True,nullable=False,default=uuid.uuid4)
+    email:Mapped[str] = mapped_column(String(255),nullable=False,unique=True,index=True)
     hashed_password:Mapped[str] = mapped_column(String(100),nullable=False)
 
+
+#for storing file data and also the owner of the file /// file could be duplicate here
 class FilesTable(Base):
     __tablename__ = "files_table"
     
     # Metadata
     id: Mapped[uuid.UUID] = mapped_column(primary_key=True, default=uuid.uuid4)
-    owner_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("users.id"))
+    owner_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("users_table.id"))
     filename: Mapped[str] = mapped_column(String(255))
     
     # B2 Data
@@ -28,22 +32,30 @@ class FilesTable(Base):
     
     # Stats
     file_size: Mapped[int] = mapped_column(BigInteger, default=0)
-    created_at: Mapped[datetime] = mapped_column(DateTime, default=lambda: datetime.now(timezone.utc))
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True),server_default=func.now())
     updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True),server_default=func.now(), onupdate=func.now())
 
+#for storing the share url links
 class FileShareTable(Base):
     __tablename__ = "file_shares_table"
 
     id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    file_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("files.id", ondelete="CASCADE"), nullable=False)
-    shared_by: Mapped[uuid.UUID] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+    file_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("files_table.id", ondelete="CASCADE"), nullable=False)
+    shared_by: Mapped[uuid.UUID] = mapped_column(ForeignKey("users_table.id", ondelete="CASCADE"), nullable=False)
 
-    # The random string for the URL (e.g., myapp.com/share/xJ92kLm0)
     # We use index=True because we will search by this token every time a link is clicked
     share_token: Mapped[str] = mapped_column(String(100), unique=True, index=True,nullable=False)
-    is_public: Mapped[bool] = mapped_column(Boolean, default=True)
     password_hash: Mapped[str | None] = mapped_column(String(255), nullable=True)
     expires_at: Mapped[datetime | None] = mapped_column(DateTime(timezone=True), nullable=True)
-    access_count: Mapped[int] = mapped_column(Integer, default=0)
+    # access_count: Mapped[int] = mapped_column(Integer, default=0)
     created_at: Mapped[datetime] = mapped_column(DateTime,default=lambda: datetime.now(timezone.utc))
 
+
+#table for sharing file with peoples
+class FilePermissionTable(Base):
+    __tablename__="file_permission_table"
+
+    id:Mapped[int]=mapped_column(Integer,primary_key=True,nullable=False,autoincrement=True)
+    user_id:Mapped[uuid.UUID] = mapped_column(ForeignKey("users_table.id",ondelete="CASCADE"))
+    file_id:Mapped[uuid.UUID]=mapped_column(ForeignKey("files_table.id",ondelete="CASCADE"))
+    granted_at:Mapped[datetime] = mapped_column(DateTime(timezone=True),server_default=func.now())
